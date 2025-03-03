6aa90e7b6f5dcc8dd656409660e554243222ec35
rebelwithoutacause <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-03 21:31:19 +0200

    Add files via upload

---
+++ 04.Building/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int countOfFloors = int.Parse(Console.ReadLine());
+int countOfRooms = int.Parse(Console.ReadLine());
+
+for (int floor = countOfFloors; floor >= 1; floor--)
+{
+    for (int room = 0; room < countOfRooms; room++)
+    {
+        if (floor == countOfFloors) // проверка дали сме на последния етаж
+        {
+            Console.Write($"L{floor}{room} ");
+        }
+        else if (floor % 2 == 0) // проветка дали сме на четен етаж
+        {
+            Console.Write($"O{floor}{room} ");
+        }
+        else if (floor % 2 == 1) // проверка дали сме на нечетен етаж
+        {
+            Console.Write($"A{floor}{room} ");
+        }
+    }
+
+    Console.WriteLine();
+}
